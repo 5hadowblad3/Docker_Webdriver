4510ef4fafcf366a63d360b6fddf1a2a6ab350d9
5hadowblad3 <568376313@qq.com>
2017-06-21 21:28:59 +0100

    robuster

--- chrome_case1.py
+++ chrome_case1.py
@@ -1,5 +1,8 @@
+import os
 import sys
 import json
+import shutil
+import urlparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -7,16 +10,19 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 
-path = './chromedriver'
+driver_path = './chromedriver'
 a = '--load-extension='
-extension_p = 'ecap.crx'
+extension_name = 'listener'
+extension_path = 'ecap.crx'
 extension_list = 'chrome://extensions-frame'
 
 url = 'http://www.baidu.com'
 
-download = './'
-prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': download}
+download_path = './json/'
+backup_path = './backup/'
+prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': download_path}
 
 start = 'start'
 stop = 'stop'
@@ -24,18 +30,17 @@ toggle = 'toggle'
 save = 'save'
 clear = 'clear'
 
-doc = '/Users/Shadow/Downloads/'
-test = 'ECAP_G_2017-06-14T12-39-51.155Z.json'
+data_set = 'dataset1'
 
 
-def get_ID(br):
+def get_ID(br, e_name):
     br.get('chrome://extensions-frame')
     names = br.find_elements_by_xpath("//div[@id='extension-settings-list']/div/div/div/div/h2")
     ID = br.find_elements_by_class_name('extension-list-item-wrapper')
 
     for ind, name in enumerate(names):
         print str(name.text), isinstance(str(name.text), str), len(str(name.text))
-        if cmp(str(name.text), 'listener') == 0:
+        if cmp(str(name.text), e_name) == 0:
             return str(ID[ind].get_attribute('id'))
 
 
@@ -48,24 +53,94 @@ def interaction(br, type):
     br.implicite_wait(5)
 
 
-def analyse_json(path):
-    fd = open(doc + test, 'r')
+def analyse_json(path, location):
+    fd = open(path, 'r')
+    fd2 = open(location, 'a')
     data = json.load(fd)
+    flag = 0
+    url = ''
+    instance = []
 
     for package in data:
         if package['type'].count('tab') > 0 or package['type'].count('navigation') > 0 \
-                or package['type'].count('window') > 0 or package['type'].count('variable') > 0:
+                or package['type'].count('window') > 0:
             continue
 
-    print data
+        # interaction with third party => tracking
+        if package['type'] == 'request':
+            if len(url) == 0:
+                url = package['details']['url']
+            elif url != str(package['details']['url']):
+                flag = 1
 
+        # upload (hidden) text
+        if package['type'] == 'variable.new' and package['details']['type'].count('input') > 0:
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
 
+        # local storage modification
+        if package['type'] == 'variable.change':
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
 
+        # user agent exchanging
+        if package['type'].count('request') > 0 and package['details']['responseHeaders'].count('user-agent'):
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
+
+        # request with cookies
+        if package['type'].count('request') > 0 and package['details']['responseHeaders'].keys().find('cookies') != -1:
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
+
+        # cookies given in the response
+        if package['type'].count('response') > 0 and \
+                        package['details']['responseHeaders'].keys().find('set-cookie') != -1:
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
+
+        # cookies modification
+        if package['type'] == 'cookie.changed':
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
+
+        # dom element changing
+        if package['type'].count('dom') > 0:
+            fd2.write('1 ')
+            instance.append(1)
+        else:
+            fd2.write('0 ')
+            instance.append(0)
+
+        # save final marke
+        fd2.write(str(flag))
+        instance.append(flag)
+
+        # save marked instance
+        data.append(instance)
+
+        return data
 
 # browser.get(extension_option)
 # browser.find_element_by_id('autoStart').click()
-
-
 # browser.get(cmd + start)
 
 
@@ -76,25 +151,24 @@ def analyse_json(path):
 if __name__ == '__main__':
 
     listener = Options()
-    listener.add_extension(extension_p)
+    listener.add_extension(extension_path)
     listener.add_experimental_option('prefs', prefs)
 
-    browser = webdriver.Chrome(path, chrome_options=listener)
+    browser = webdriver.Chrome(driver_path, chrome_options=listener)
     print 'enter'
-    print get_ID(browser)
+    print get_ID(browser, extension_name)
 
-    cmd = 'chrome-extension://' + get_ID(browser) + '/command.html?'
-    cmd = 'chrome-extension://' + get_ID(browser) + '/command.html?'
+    cmd = 'chrome-extension://' + get_ID(browser, extension_name) + '/command.html?'
 
     # search = browser.find_element_by_id("kw")
     # search.send_keys('test')
-
 
     # send_cmd(browser, cmd + start)
 
     # case = sys.argv[1]
 
     # analyse_json(path + test)
+
     # print 'hello world'
     # Need modified
     # fd = open('shoes_case1.txt')
@@ -102,10 +176,11 @@ if __name__ == '__main__':
     #     send_cmd(browser, cmd + clear)
     #     browser.get(i)
     #     send_cmd(browser, cmd + save)
-    #     analyse_json(download)
+    #     json_file = os.listdir(download_path)[0]
+    #     analyse_json(download_path + json_file)
+    #     shutil.move(download_path + json_file, backup_path + json_file)
 
     # print browser.title
     # print browser.page_source
 
-
-    browser.quit()
+    # browser.quit()
--- crawl.py
+++ crawl.py
@@ -5,24 +5,24 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 path = '/Users/Shadow/Downloads/Imperial/Individual/selenium/chromedriver'
 
-extension_p = '/Users/Shadow/Downloads/Imperial/Individual/selenium/ecap.crx'
-
-url = 'http://www.ranker.com/list/the-best-shoe-websites/sirsinister'
+url = 'https://builtwith.com/ecommerce/united-states/shoes'
 
 class_name = 'listItem__properties black default'
+
+f = open('shoes_case1.txt', 'a')
 
 browser = webdriver.Chrome(path)
 
 browser.get(url)
 
-print browser.page_source
-browser.f
-lists = browser.find_element_by_class_name(class_name)
-
+lists = browser.find_elements_by_xpath("//table[@class='table table-striped tablePhone']/tbody/tr/td/img")
 
 for item in lists:
-    print 1
-    print item
+    print item.get_attribute('src')
+    source = str(item.get_attribute('src'))
+    print
+    f.write(source[source.index('=') + 1:] + '\n')
+    # f.write(str(item.get_attribute('href')) + '\n')
 
 
 
